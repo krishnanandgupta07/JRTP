b3a4197a876731faac45fc15175747adf094ebe0
Krishna Nand Gupta <krishna1605gupta@gmail.com>
2024-08-27 13:58:17 +0530

    TL-created a new branch payment and create a new UPIPayment.java class

---
+++ GITProj01-ByUsingTwoDevloper/src/main/java/com/nt/payment/UPIPayment.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class UPIPayment {
+
+	public String doPayment(long phoneNumber,double amount) {
+		System.out.println("Payment is done");
+		return "Payment is done using UPI";
+	}
+}
